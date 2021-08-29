1ce922a57c075853f8547572045f69c282545080
Patricia Souza <patricia.asouza@mercadolivre.com>
2021-08-29 20:43:49 -0300

    Update versao final

---
+++ src/javaoop/SocorristaCarro.java
@@ -0,0 +1,8 @@
+package javaoop;
+
+public class SocorristaCarro {
+
+    public void socorrer(Carro carro){
+        System.out.println("Socorrendo carro >> " + carro.getPlaca());
+    }
+}
---
+++ src/javaoop/SocorristaMoto.java
@@ -0,0 +1,8 @@
+package javaoop;
+
+public class SocorristaMoto {
+
+    public void socorrer(Moto moto){
+        System.out.println("Socorrendo moto >> " + moto.getPlaca());
+    }
+}
